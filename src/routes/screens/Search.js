fc385b1cd78b03221a06768cb5ec58df7eae5a9d
Tharegorn <jamie-antoine.ponchon@epitech.eu>
2021-10-25 16:33:48 +0200

    Possibility to search subreddits, get informations of them and check all posts

--- src/routes/screens/Search.js
+++ src/routes/screens/Search.js
@@ -3,31 +3,50 @@ import {
     Text,
     Button,
     Image,
+    TouchableOpacity,
+    ScrollView
 } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { useAuth } from '../utils/useauth';
+import { Card } from 'react-native-elements'
 import axios from 'axios';
 
 const Search = () => {
     const auth = useAuth();
     const [search, setSearch] = useState("");
-    const [infos, setInfos] = useState(null)
+    const [infos, setInfos] = useState(null);
+    const [posts, setPosts] = useState(null);
     function change(e) {
         setSearch(e)
+        setPosts(null)
         axios.get("https://www.reddit.com/r/" + e + "/about.json")
             .then(response => {
                 const data = response.data.data
-                console.log(response.data.data)
+                if (data.url != null) {
                 setInfos({
                     accept_follows: data.accept_followers,
-                    name: data.display_name,
-                    sub_name: data.display_name_prefixed,
-                    description: data.submit_text
+                    name: data.title,
+                    sub_name: data.url,
+                    header: data.header_img,
+                    followers: data.subscribers,
+                    online: data.active_user_count,
+                    description: data.public_description
                 })
-                // setPosts(response.data.data.children)
+            } else {
+                setInfos(null)
+            }
             })
             .catch(error => {
                 setInfos(null)
+            })
+    }
+    function getPosts() {
+        axios.get("https://www.reddit.com/r/" + search + "/top.json?limit=100")
+            .then(response => {
+                setPosts(response.data.data.children)
+            })
+            .catch(error => {
+                setPosts()
             })
     }
     return (
@@ -37,12 +56,32 @@ const Search = () => {
                 value={search} />
             {infos == null ?
                 <Text>Any subreddit was found</Text> : <>
-                    <Text>Founded</Text>
-                    <Text>Name : {infos.name}</Text>
-                    <Text>SubReddit : {infos.sub_name}</Text>
-                    <Text>Follows : {infos.accept_follows}</Text>
-                    <Text>Desc : {infos.submit_text}</Text>
-                    </>}
+                    <TouchableOpacity onPress={() => getPosts()}>
+                        <Card>
+                            <Card.FeaturedTitle>{infos.name}</Card.FeaturedTitle>
+                            <Card.Title>{infos.sub_name}</Card.Title>
+                            <Card.Title>Follows : {infos.followers}</Card.Title>
+                            <Card.Title>Online : {infos.online}</Card.Title>
+                            <Card.Divider />
+                            <Text>Description : {infos.description}</Text>
+                        </Card>
+                    </TouchableOpacity>
+                    <ScrollView>
+                        {posts ? posts.map((item, index) => (
+                            <Card key={index}>
+                                <Card.Title>/r/{item.data.subreddit}</Card.Title>
+                                <Card.Title>{item.data.title}</Card.Title>
+                                <Card.Title>{item.data.created}</Card.Title>
+                                <Card.Divider />
+                                {item.data.thumbnail ?
+                                    <Card.Image source={{ uri: item.data.thumbnail }} style={{ height: item.data.thumbnail_height, width: item.data.thumbnail_width }}>
+                                    </Card.Image> : <Text>NoImage</Text>}
+                                {/* <Text>https://reddit.com{item.data.permalink}</Text> */}
+                                <Text>{item.data.num_comments} Comments</Text>
+                            </Card>
+                        )) : <Text></Text>}
+                    </ScrollView>
+                </>}
         </>
 
     )
