5408477304fe8de8ed5fa1257655ac1f8e1b09d7
Tharegorn <jamie-antoine.ponchon@epitech.eu>
2021-10-25 20:35:06 +0200

    Add some front in connection page

--- src/routes/screens/Connection.js
+++ src/routes/screens/Connection.js
@@ -3,13 +3,20 @@ import {
     Text,
     Button,
     Image,
+    View
 } from 'react-native';
 import { useAuth } from '../utils/useauth';
+import Video from 'react-native-video'
 
 const Connect = () => {
     const auth = useAuth();
 
-    return (<Button title="Connect to reddit account" onPress={() => { auth.Authenticate() }}></Button>)
+    return (
+        <View style={{ backgroundColor: 'rgba(255,70,30,1)', flex: 1 }}>
+            <Image source={require("../assets/reddit-logo-16.png")} style={{ height: 200, width: 200, alignSelf: 'center' }} />
+            <Text style={{ color: 'black', fontSize: 30, alignSelf: 'center', fontFamily: 'Neo Sans' }}>Gayguette</Text>
+            <Button title="Connect to reddit account" onPress={() => { auth.Authenticate() }}></Button>
+        </View>)
 }
 
 export default Connect;
