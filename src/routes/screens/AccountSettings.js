4b66d7cca62650f2b531c44c417d8943265caf8f
Tharegorn <jamie-antoine.ponchon@epitech.eu>
2021-10-25 18:19:46 +0200

    Possibility to modify parameters but now there is a 403 error

--- src/routes/screens/AccountSettings.js
+++ src/routes/screens/AccountSettings.js
@@ -5,7 +5,8 @@ import {
     Image,
 } from 'react-native';
 import { useAuth } from '../utils/useauth';
-import {Switch} from 'react-native-elements'
+import { Switch } from 'react-native-elements'
+import axios from 'axios';
 
 const Settings = () => {
     const auth = useAuth();
@@ -16,9 +17,41 @@ const Settings = () => {
         <>
             <Text>Change Profile settings</Text>
             <Text>Accept pms from : {auth.prefs.pms}</Text>
-            <Text>Show presence : <Switch value={auth.prefs.presence}/></Text>
-            <Text>Over 18: <Switch value={auth.prefs.over_18}/></Text>
-            <Text>label nsfw: <Switch value={auth.prefs.label_nsfw}/></Text>
+            <Text>Show presence : <Switch value={auth.prefs.presence} /></Text>
+            <Text>Over 18: <Switch value={auth.prefs.over_18} onChange={() => {
+                console.log(auth.token)
+                if (auth.prefs.over_18) {
+                    const options = {
+                        method: 'PATCH',
+                        url: 'https://oauth.reddit.com/api/v1/me/prefs',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            Authorization: "Bearer " + auth.token,
+                        }, data: {
+                            over_18: false
+                        }
+                    };
+                    axios.request(options).then((res) => {
+                        console.log(response)
+                    }).catch((a) => {
+                        console.error(a)
+                    })
+                } else {
+                    const options = {
+                        method: 'PATCH',
+                        url: 'https://oauth.reddit.com/api/v1/me/prefs',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            Authorization: "Bearer " + auth.token,
+                        }, data: {
+                            over_18: true,
+                        }
+                    };
+                    axios.request(options)
+                }
+                auth.Prefs()
+            }} /></Text>
+            <Text>label nsfw: <Switch value={auth.prefs.label_nsfw} /></Text>
             <Text>Lang: {auth.prefs.lang}</Text>
             <Text>Description: </Text>
         </>
