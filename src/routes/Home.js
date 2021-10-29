a4d6cddacad527f0c714ab3784b68d70ea860189
Sebastiensrn <64637040+sebastiensrn@users.noreply.github.com>
2021-10-29 01:37:06 +0100

    parsing of back end for better UI integration and HomePage front

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,68 +1,112 @@
-
-import React, { useState, useEffect } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomePage from './screens/HomePage'
+import React, {useState, useEffect, StyleSheet} from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomePage from './screens/HomePage';
 import Profile from './screens/Profile';
 import Connect from './screens/Connection';
 import Search from './screens/Search';
 import Settings from './screens/AccountSettings';
-import list from './screens/list'
-import { useAuth } from './utils/useauth';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import list from './screens/list';
+import {useAuth} from './utils/useauth';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = ({ state, descriptor, navigation }) => {
-    const auth = useAuth();
+const Home = () => {
+  const auth = useAuth();
 
-    return (
-        <>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomePage} options={{
-                    headerShown: false, tabBarLabel: '',
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="home" color={color} size={size} />
-                    ),
-                }}
+  return (
+    <>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomePage}
+          options={{
+            headerShown: false,
+            tabBarLabel: '',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            headerShown: false,
+            tabBarLabel: '',
+            // tabBarIcon: ({ color, size }) => (
+            // <MaterialCommunityIcons name="search" color={color} size={size} />
+            // ),
+          }}
+        />
+        {auth.token != null ? (
+          <>
+            <Tab.Screen
+              name="Profile"
+              component={Profile}
+              options={{
+                tabBarLabel: '',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                  <MaterialCommunityIcons
+                    name="account"
+                    color={color}
+                    size={size}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={Settings}
+              options={{
+                tabBarLabel: '',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                  <MaterialCommunityIcons
+                    name="account-settings-outline"
+                    color={color}
+                    size={size}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Another"
+              component={list}
+              options={{
+                tabBarLabel: '',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                  <MaterialCommunityIcons
+                    name="account-settings-outline"
+                    color={color}
+                    size={size}
+                  />
+                ),
+              }}
+            />
+          </>
+        ) : (
+          <Tab.Screen
+            name="Connect"
+            component={Connect}
+            options={{
+              tabBarLabel: '',
+              headerShown: false,
+              tabBarIcon: ({color, size}) => (
+                <MaterialCommunityIcons
+                  name="account-plus"
+                  color={color}
+                  size={size}
                 />
-                <Tab.Screen name="Search" component={Search} options={{
-                    headerShown: false, tabBarLabel: ''
-                    // tabBarIcon: ({ color, size }) => (
-                    // <MaterialCommunityIcons name="search" color={color} size={size} />
-                    // ),
-                }}
-                />
-                {auth.token != null ?
-                    <>
-                        <Tab.Screen name="Profile" component={Profile} options={{
-                            tabBarLabel: '', headerShown: false,
-                            tabBarIcon: ({ color, size }) => (
-                                <MaterialCommunityIcons name="account" color={color} size={size} />
-                            ),
-                        }} />
-                        <Tab.Screen name="Settings" component={Settings} options={{
-                            tabBarLabel: '', headerShown: false,
-                            tabBarIcon: ({ color, size }) => (
-                                <MaterialCommunityIcons name="account-settings-outline" color={color} size={size} />
-                            ),
-                        }} />
-                        <Tab.Screen name="Another" component={list} options={{
-                            tabBarLabel: '', headerShown: false,
-                            tabBarIcon: ({ color, size }) => (
-                                <MaterialCommunityIcons name="account-settings-outline" color={color} size={size} />
-                            ),
-                        }} />
-                    </> :
-                    <Tab.Screen name="Connect" component={Connect} options={{
-                        tabBarLabel: '', headerShown: false,
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="account-plus" color={color} size={size} />
-                        ),
-                    }} />
-                }
-            </Tab.Navigator>
-        </>
-    )
+              ),
+            }}
+          />
+        )}
+      </Tab.Navigator>
+    </>
+  );
 };
 
-export default HomeScreen;
+export default Home;
--- src/routes/screens/HomePage.js
+++ src/routes/screens/HomePage.js
@@ -1,9 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, ScrollView, Button} from 'react-native';
-import {Card, ButtonGroup} from 'react-native-elements';
+import {
+  Text,
+  View,
+  ScrollView,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  ImageBackground,
+} from 'react-native';
+import Header from '../navigation/Header';
 import axios from 'axios';
+import {Card, ButtonGroup} from 'react-native-elements';
 
-const HomePage = navigation => {
+const HomePage = () => {
   const [posts, setPosts] = useState();
   const [bttIndex, setIndex] = useState(0);
   const map = {0: 'new', 1: 'rising', 2: 'best', 3: 'top', 4: 'hot'};
@@ -24,40 +34,78 @@ const HomePage = navigation => {
   return (
     <View>
       <ScrollView>
-        <ButtonGroup
-          buttons={['New', 'Rising', 'Best', 'Top', 'Hot']}
-          selectedIndex={bttIndex}
-          onPress={e => {
-            setIndex(e);
-            load(e);
-          }}
-        />
-        {posts ? (
-          posts.map((item, index) => (
-            <Card key={index}>
-              <Card.Title>/r/{item.data.subreddit}</Card.Title>
-              <Card.Title>{item.data.title}</Card.Title>
-              <Card.Title>{item.data.created}</Card.Title>
-              <Card.Divider />
-              {item.data.thumbnail ? (
-                <Card.Image
-                  source={{uri: item.data.thumbnail}}
-                  style={{
-                    height: item.data.thumbnail_height,
-                    width: item.data.thumbnail_width,
-                  }}></Card.Image>
-              ) : (
-                <Text>NoImage</Text>
-              )}
-              {/* <Text>https://reddit.com{item.data.permalink}</Text> */}
-              <Text>{item.data.num_comments} Comments</Text>
-            </Card>
-          ))
-        ) : (
-          <Text>Nothing</Text>
-        )}
+        <View style={styles.container}>
+          <Image
+            source={require('../assets/FrameOrange.png')}
+            style={{
+              position: 'absolute',
+              height: 200,
+              width: '100%',
+              zIndex: 0,
+              left: 0,
+              right: 0,
+            }}
+          />
+          <Header />
+          <ButtonGroup
+            buttons={['New', 'Rising', 'Best', 'Top', 'Hot']}
+            selectedIndex={bttIndex}
+            onPress={e => {
+              setIndex(e);
+              load(e);
+            }}
+          />
+          {posts ? (
+            posts.map((item, index) => (
+              <Card containerStyle={styles.cardStyle} key={index}>
+                <Card.Title>
+                  <Text style={styles.paragraph}>/r/{item.data.subreddit}</Text>
+                </Card.Title>
+                {item.data.thumbnail ? (
+                  <Card.Image
+                    source={{uri: item.data.thumbnail}}
+                    style={{
+                      width: '100%',
+                    }}></Card.Image>
+                ) : (
+                  <Text>NoImage</Text>
+                )}
+                <Card.Title>{item.data.title}</Card.Title>
+                <Card.Title>{item.data.created}</Card.Title>
+
+                {/* <Text>https://reddit.com{item.data.permalink}</Text> */}
+                <Text>{item.data.num_comments} Comments</Text>
+              </Card>
+            ))
+          ) : (
+            <Text>Nothing</Text>
+          )}
+        </View>
       </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#101111',
+    flex: 1,
+  },
+  cardStyle: {
+    borderRadius: 6,
+    borderWidth: 0,
+    backgroundColor: 'rgba(255, 255, 255, 0.04)',
+    shadowColor: 'rgba(0,0,0,0)',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0,
+  },
+  paragraph: {
+    color: '#FFF',
+    paddingLeft: 0,
+  },
+});
+
 export default HomePage;
